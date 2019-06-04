7352c1847c93d4480241e2a3fc4de38e05267b1a
pbhandar2 <bhandaripranav94@gmail.com>
2019-06-03 21:13:46 -0400

    add test case to start with

--- README.md
+++ README.md
@@ -4,4 +4,23 @@ A framework for analysis of block I/O traces. Configure the trace_config.json to
 
 Currently the workload metrics that it gives are: 
 
-1. Rate of read/write 
+self.metrics = {
+	"read_count": 0,
+	"write_count": 0,
+	"total_io": 0,
+	"read_write_ratio": None,
+	"read_rate": None,
+	"write_rate": None,
+	"io_rate": None,
+	"total_io_size": 0,
+	"total_read_size": 0,
+	"total_write_size": 0,
+	"average_read_size": None,
+	"average_write_size": None,
+	"average_io_size": None,
+	"min_read_size": None,
+	"min_write_size": None,
+	"max_read_size": None,
+	"max_write_size": None
+}
+
--- test/tester.py
+++ test/tester.py
@@ -1,22 +1,23 @@
-import sys
+import sys, json
 
 sys.path.append('../')
 
 from skeletor import Skeletor
 
-@profile
+#@profile
 def my_func():
 	processor = Skeletor()
-	processor.open_file("web_0.csv.gz", "../trace_config.json", "MSR-Cambridge")
+	processor.open_file("web_1.csv.gz", "../trace_config.json", "MSR-Cambridge")
 	profiler = processor.get_metric_extractor()
 	profiler.extract_metric()
 
-	print(len(profiler.reader.data["io_type"]))
-	print(len(profiler.reader.data["time"]))
-	print(profiler.reader.data["io_type"][1:10])
-	print(profiler.reader.clock.time_elasped)
+	# print(len(profiler.reader.data["io_type"]))
+	# print(len(profiler.reader.data["time"]))
+	# print(profiler.reader.data["io_type"][1:10])
+	# print(profiler.reader.clock.time_elasped)
 
-	print(profiler.metrics)
+	print("Here are the metrics!")
+	print(json.dumps(profiler.metrics, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
