f8137087eaee205ba7ac9270ab6a0d935a73eef1
pbhandar2 <bhandaripranav94@gmail.com>
2019-07-04 17:02:38 -0400

    fix the name in the msr test file

--- test/msr_test.py
+++ test/msr_test.py
@@ -2,18 +2,17 @@ from skeletor import Skeletor
 import sys
 import pdb
 
-sys.path.append('../')
-
 
 def main(file_loc):
     processor = Skeletor()
-    processor.open_file(file_loc, "../trace_config.json", "MSR")
+    processor.open_file(file_loc, "../trace_config.json", "MSR-Cambridge")
     profiler = processor.get_metric_extractor()
     profiler.extract_metric()
     pdb.set_trace()
 
 
 if __name__ == "__main__":
+    sys.path.append('../')
     file_loc = sys.argv[1]
     main(file_loc)
 
