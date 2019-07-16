68d6055d5208cfc3c323ca522f0bade6e62f9571
pbhandar2 <bhandaripranav94@gmail.com>
2019-07-16 15:02:18 -0400

    add tsfresh script

--- scripts/tsfresh_extract.py
+++ scripts/tsfresh_extract.py
@@ -1,7 +1,7 @@
 from tsfresh.feature_extraction import extract_features, EfficientFCParameters, ComprehensiveFCParameters
 import sys
 import pandas as pd
-import numpy as np 
+import numpy as np
 
 
 def main():
@@ -15,10 +15,12 @@ def main():
         "id": np.ones(limit),
         "time": np.arange(limit)
     });
+
     extracted_features = extract_features(ts, column_id="id", column_sort="time", column_value="ts", n_jobs=8, show_warnings=False,
                                                           default_fc_parameters=EfficientFCParameters())
 
     extracted_features.to_csv(sys.argv[2])
+
 
 if __name__ == "__main__":
     main()
