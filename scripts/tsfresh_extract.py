281359d538951cd409993aa7aeb86c9a96a7f9d9
pbhandar2 <bhandaripranav94@gmail.com>
2019-07-16 15:24:05 -0400

    add tsfresh script

--- scripts/tsfresh_extract.py
+++ scripts/tsfresh_extract.py
@@ -7,16 +7,26 @@ import numpy as np
 def main():
 
     ts_file_loc = sys.argv[1]
-    df = pd.read_csv(ts_file_loc, names=["ts"], dtype={"ts": int})
+    df = pd.read_csv(ts_file_loc, names=["ts"], dtype={"ts": int, "id": int, "time": int})
+    # df.insert(0, "time", pd.Series(range(0, len(df)), dtype='int32'))
+    # df.insert(1, "id", pd.Series([1]*len(df), dtype='int32'))
 
-    limit=len(df)
-    ts = pd.DataFrame({
-        "ts": df[:limit],
-        "id": np.ones(limit),
-        "time": np.arange(limit)
-    });
+    print(df.head())
 
-    extracted_features = extract_features(ts, column_id="id", column_sort="time", column_value="ts", n_jobs=8, show_warnings=False,
+    # limit = 2
+    # id_array = np.ones(limit)
+    # ts_array = np.random.rand(1, limit)
+    # print(df)
+    #
+    # ts = pd.DataFrame({
+    #     "ts": np.reshape(df.values[:limit], (-1, limit))[0],
+    #     "id": np.reshape(id_array, (-1, limit))[0],
+    #     "time": range(limit)
+    # });
+
+
+
+    extracted_features = extract_features(df, column_id="id", column_sort="time", column_value="ts", n_jobs=8, show_warnings=False,
                                                           default_fc_parameters=EfficientFCParameters())
 
     extracted_features.to_csv(sys.argv[2])
